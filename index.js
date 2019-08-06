f4610e9e5c1a65402eaca50300fb4dcfbd8033e4
pennert-1630865 <pennert@uw.edu>
2019-08-06 11:52:38 -0700

    testing push

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+var express = require("express");
+var ParseServer = require("parse-server").ParseServer;
+var path = require("path");
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-
-if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
-}
+var databaseUri =
+  "mongodb://heroku_wb573k89:iq0taj0p11iko36o3sii4a1928@ds259207.mlab.com:59207/heroku_wb573k89";
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  databaseURI: databaseUri || "mongodb://localhost:27017/dev",
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + "/cloud/main.js",
+  appId: process.env.APP_ID || "tfp-parse-practice",
+  masterKey: process.env.MASTER_KEY || "master", //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || "http://localhost:1337/parse", // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -28,27 +25,27 @@ var api = new ParseServer({
 var app = express();
 
 // Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use("/public", express.static(path.join(__dirname, "/public")));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+var mountPath = process.env.PARSE_MOUNT || "/parse";
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+app.get("/", function(req, res) {
+  res.status(200).send("_________TEST_________");
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+app.get("/test", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/test.html"));
 });
 
 var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
+var httpServer = require("http").createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+  console.log("parse-server-example running on port " + port + ".");
 });
 
 // This will enable the Live Query real-time server
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,46 +6,52 @@ var Steps = {};
 
 Steps.init = function() {
   this.buildParseUrl();
-  this.bindBtn('#step-1-btn', function(e){
+  this.bindBtn("#step-1-btn", function(e) {
     ParseRequest.postData();
     e.preventDefault();
-  })
-}
+  });
+};
 
 Steps.buildParseUrl = function() {
   var url = Config.getUrl();
-  $('#parse-url').html(url + '/parse');
-}
+  $("#parse-url").html(url + "/parse");
+};
 
 Steps.bindBtn = function(id, callback) {
   $(id).click(callback);
-}
+};
 
 Steps.closeStep = function(id) {
-  $(id).addClass('step--disabled');
-}
+  $(id).addClass("step--disabled");
+};
 
-Steps.openStep  = function(id) {
-  $(id).removeClass('step--disabled');
-}
+Steps.openStep = function(id) {
+  $(id).removeClass("step--disabled");
+};
 
-Steps.fillStepOutput  = function(id, data) {
-  $(id).html('Output: ' + data).slideDown();
-}
+Steps.fillStepOutput = function(id, data) {
+  $(id)
+    .html("Output: " + data)
+    .slideDown();
+};
 
-Steps.fillStepError  = function(id, errorMsg) {
-  $(id).html(errorMsg).slideDown();
-}
+Steps.fillStepError = function(id, errorMsg) {
+  $(id)
+    .html(errorMsg)
+    .slideDown();
+};
 
-
-Steps.fillBtn  = function(id, message) {
-  $(id).addClass('success').html('✓  ' + message);
-}
+Steps.fillBtn = function(id, message) {
+  $(id)
+    .addClass("success")
+    .html("✓  " + message);
+};
 
 Steps.showWorkingMessage = function() {
-  $('#step-4').delay(500).slideDown();
-}
-
+  $("#step-4")
+    .delay(500)
+    .slideDown();
+};
 
 /**
  *  Parse requests handler
@@ -54,60 +60,65 @@ Steps.showWorkingMessage = function() {
 var ParseRequest = {};
 
 ParseRequest.postData = function() {
-  XHR.setCallback(function(data){
-    // store objectID
-    Store.objectId = JSON.parse(data).objectId;
-    // close first step
-    Steps.closeStep('#step-1');
-    Steps.fillStepOutput('#step-1-output', data);
-    Steps.fillBtn('#step-1-btn', 'Posted');
-    // open second step
-    Steps.openStep('#step-2');
-    Steps.bindBtn('#step-2-btn', function(e){
-      ParseRequest.getData();
-      e.preventDefault();
-    });
-  },
-  function(error) {
-       Steps.fillStepError('#step-1-error', 'There was a failure: ' + error);
-   });
-  XHR.POST('/parse/classes/GameScore');
-};
-
-ParseRequest.getData = function() {
-  XHR.setCallback(function(data){
-    // close second step
-    Steps.closeStep('#step-2');
-    Steps.fillStepOutput('#step-2-output', data);
-    Steps.fillBtn('#step-2-btn', 'Fetched');
-    // open third step
-    Steps.openStep('#step-3');
-    Steps.bindBtn('#step-3-btn', function(e){
-      ParseRequest.postCloudCodeData();
-      e.preventDefault();
+  XHR.setCallback(
+    function(data) {
+      // store objectID
+      Store.objectId = JSON.parse(data).objectId;
+      // close first step
+      Steps.closeStep("#step-1");
+      Steps.fillStepOutput("#step-1-output", data);
+      Steps.fillBtn("#step-1-btn", "Posted");
+      // open second step
+      Steps.openStep("#step-2");
+      Steps.bindBtn("#step-2-btn", function(e) {
+        ParseRequest.getData();
+        e.preventDefault();
       });
     },
     function(error) {
-    	Steps.fillStepError('#step-2-error', 'There was a failure: ' + error);
-  });  
-  XHR.GET('/parse/classes/GameScore');
+      Steps.fillStepError("#step-1-error", "There was a failure: " + error);
+    }
+  );
+  XHR.POST("/parse/classes/GameScore");
+};
+
+ParseRequest.getData = function() {
+  XHR.setCallback(
+    function(data) {
+      // close second step
+      Steps.closeStep("#step-2");
+      Steps.fillStepOutput("#step-2-output", data);
+      Steps.fillBtn("#step-2-btn", "Fetched");
+      // open third step
+      Steps.openStep("#step-3");
+      Steps.bindBtn("#step-3-btn", function(e) {
+        ParseRequest.postCloudCodeData();
+        e.preventDefault();
+      });
+    },
+    function(error) {
+      Steps.fillStepError("#step-2-error", "There was a failure: " + error);
+    }
+  );
+  XHR.GET("/parse/classes/GameScore");
 };
 
 ParseRequest.postCloudCodeData = function() {
-  XHR.setCallback(function(data){
-    // close second step
-    Steps.closeStep('#step-3');
-    Steps.fillStepOutput('#step-3-output', data);
-    Steps.fillBtn('#step-3-btn', 'Tested');
-    // open third step
-    Steps.showWorkingMessage();
+  XHR.setCallback(
+    function(data) {
+      // close second step
+      Steps.closeStep("#step-3");
+      Steps.fillStepOutput("#step-3-output", data);
+      Steps.fillBtn("#step-3-btn", "Tested");
+      // open third step
+      Steps.showWorkingMessage();
     },
     function(error) {
-    	Steps.fillStepError('#step-3-error', 'There was a failure: ' + error);
-    });  
-  XHR.POST('/parse/functions/hello');
-}
-
+      Steps.fillStepError("#step-3-error", "There was a failure: " + error);
+    }
+  );
+  XHR.POST("/parse/functions/hello");
+};
 
 /**
  * Store objectId and other references
@@ -122,11 +133,10 @@ var Config = {};
 Config.getUrl = function() {
   if (url) return url;
   var port = window.location.port;
-  var url = window.location.protocol + '//' + window.location.hostname;
-  if (port) url = url + ':' + port;
+  var url = window.location.protocol + "//" + window.location.hostname;
+  if (port) url = url + ":" + port;
   return url;
-}
-
+};
 
 /**
  * XHR object
@@ -146,23 +156,22 @@ XHR.setCallback = function(callback, failureCallback) {
       }
     }
   };
-}
+};
 
 XHR.POST = function(path, callback) {
-  var seed = {"score":1337,"playerName":"Sean Plott","cheatMode":false}
+  var seed = { score: 1337, playerName: "Sean Plott", cheatMode: false };
   this.xhttp.open("POST", Config.getUrl() + path, true);
-  this.xhttp.setRequestHeader("X-Parse-Application-Id", $('#appId').val());
+  this.xhttp.setRequestHeader("X-Parse-Application-Id", $("#appId").val());
   this.xhttp.setRequestHeader("Content-type", "application/json");
   this.xhttp.send(JSON.stringify(seed));
-}
+};
 
 XHR.GET = function(path, callback) {
-  this.xhttp.open("GET", Config.getUrl() + path + '/' + Store.objectId, true);
-  this.xhttp.setRequestHeader("X-Parse-Application-Id", $('#appId').val());
+  this.xhttp.open("GET", Config.getUrl() + path + "/" + Store.objectId, true);
+  this.xhttp.setRequestHeader("X-Parse-Application-Id", $("#appId").val());
   this.xhttp.setRequestHeader("Content-type", "application/json");
   this.xhttp.send(null);
-}
-
+};
 
 /**
  *  Boot
